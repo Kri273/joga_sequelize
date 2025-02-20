49fb5736670787fb3bf19fdbf2da2236dbb70dc6
Kri273 <kristelle.tasane@voco.ee>
2025-02-20 09:57:38 +0200

    Created new article

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ sequelize
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
 app.use('/article', articleRouter);
+app.use('admin/article', articleRouter)
 
 
 app.listen(3000, () => {
